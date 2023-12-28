d94e9776f9939662294683e68f3aaa62a866bcad
Goidan Matei-Constantin <goidan.mateiconstantin@gmail.com>
2023-12-29 01:05:12 +0200

    Update and rename NonRepetitiveBallSchema.R to NonRepetitiveBallScheme.R

--- NonRepetitiveBallScheme.R
+++ NonRepetitiveBallScheme.R
@@ -1,9 +1,9 @@
-# Non-repetitive ball schema
+# Non-repetitive ball scheme
 SBN <- function(n1, n2, N1, N2) {
   return ((choose(N1, n1) * choose(N2, n2)) / choose(N1 + N2, n1 + n2))
 }
 
-# Expected value of the non-repetitive ball schema
+# Expected value of the non-repetitive ball scheme
 ESBN <- function(n, N) {
   if (length(n) != length(N)) {
     return("Incorrect")
